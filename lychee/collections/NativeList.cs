e8f9802ee2341d1c8ad26ca680a0c40ef8cb7696
ymyh <2318749984@qq.com>
2025-09-22 17:55:47 +0800

    add NativeList.cs

---
+++ lychee/collections/NativeList.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace lychee.collections;
+
+public sealed class NativeList<T> : IDisposable, IEnumerable<T> where T : unmanaged
+{
+    private unsafe T* data;
+
+    private int size;
+
+    private int capacity;
+
+    public bool IsFull => size == capacity;
+
+    public bool IsEmpty => size == 0;
+
+    public int Count => size;
+
+    public int Capacity
+    {
+        get => capacity;
+        set => EnsureCapacity(value);
+    }
+
+    public ref T this[int index]
+    {
+        get
+        {
+            unsafe
+            {
+                return ref data[index];
+            }
+        }
+    }
+
+    public NativeList()
+    {
+        EnsureCapacity(16);
+    }
+
+    public NativeList(int capacity)
+    {
+        EnsureCapacity(capacity);
+    }
+
+    public NativeList(T[] array)
+    {
+        EnsureCapacity(array.Length);
+        size = array.Length;
+
+        unsafe
+        {
+            var span = new Span<T>(data, array.Length);
+            array.CopyTo(span);
+        }
+    }
+
+    ~NativeList()
+    {
+        Dispose();
+    }
+
+    public void Add(in T item)
+    {
+        if (IsFull)
+        {
+            EnsureCapacity(capacity * 2);
+        }
+
+        unsafe
+        {
+            data[size++] = item;
+        }
+    }
+
+    public void Clear()
+    {
+        size = 0;
+    }
+
+    public void Fill(in T item)
+    {
+        unsafe
+        {
+            var span = new Span<T>(data, size);
+            span.Fill(item);
+        }
+    }
+
+    public void Fill(int begin, int end, in T item)
+    {
+        if ((uint)begin > (uint)end || (uint)end > (uint)size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(begin) + "/" + nameof(end), "Invalid range specified");
+        }
+
+        unsafe
+        {
+            var span = new Span<T>(data, size);
+            span.Slice(begin, end - begin).Fill(item);
+        }
+    }
+
+    public void Remove(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        if (index == size - 1)
+        {
+            RemoveLast();
+            return;
+        }
+
+        unsafe
+        {
+            for (var i = index; i < size - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+        }
+
+        size--;
+    }
+
+    public void RemoveLast()
+    {
+        if (size == 0)
+        {
+            throw new InvalidOperationException("List is empty");
+        }
+
+        size--;
+    }
+
+    public void Resize(int newLength, in T item)
+    {
+        if (newLength < size)
+        {
+            size = newLength;
+            return;
+        }
+
+        EnsureCapacity(newLength);
+
+        unsafe
+        {
+            var span = new Span<T>(data, newLength);
+            span.Slice(size, newLength - size).Fill(item);
+        }
+
+        size = newLength;
+    }
+
+    public void ShirkToFit()
+    {
+        if (capacity > size)
+        {
+            unsafe
+            {
+                if (size != 0)
+                {
+                    var ptr = NativeMemory.Alloc((nuint)(size * sizeof(T)));
+                    NativeMemory.Copy(data, ptr, (nuint)(size * sizeof(T)));
+                    NativeMemory.Free(data);
+
+                    data = (T*)ptr;
+                }
+                else
+                {
+                    NativeMemory.Free(data);
+                    data = null;
+                    capacity = 16;
+                }
+            }
+        }
+    }
+
+    private unsafe void EnsureCapacity(int capacity)
+    {
+        if (capacity < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(capacity), "Capacity must be non-negative");
+        }
+
+        if (capacity < this.capacity)
+        {
+            return;
+        }
+
+        this.capacity = Math.Max(this.capacity * 2, capacity);
+
+        if (data != null)
+        {
+            var ptr = NativeMemory.Alloc((nuint)(this.capacity * sizeof(T)));
+            NativeMemory.Copy(data, ptr, (nuint)(size * sizeof(T)));
+            NativeMemory.Free(data);
+
+            data = (T*)ptr;
+        }
+        else
+        {
+            data = (T*)NativeMemory.Alloc((nuint)(this.capacity * sizeof(T)));
+        }
+    }
+
+    public void Dispose()
+    {
+        unsafe
+        {
+            if (data != null)
+            {
+                NativeMemory.Free(data);
+                data = null;
+                size = 0;
+                capacity = 0;
+            }
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new Enumerator(this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)this).GetEnumerator();
+    }
+
+    public struct Enumerator(NativeList<T> list) : IEnumerator<T>
+    {
+        private int index = -1;
+
+        public T Current => list[index];
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < list.size;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- lychee/collections/SparseMap.cs
+++ lychee/collections/SparseMap.cs
@@ -1,15 +1,17 @@
-﻿namespace lychee.collections;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace lychee.collections;
 
 public interface AsInt
 {
     int AsInt();
 }
 
-public sealed class SparseMap<T> where T : AsInt
+public sealed class SparseMap<T> : IDisposable where T : AsInt
 {
 #region Private fields
 
-    private readonly List<int> sparseArray = [];
+    private readonly NativeList<int> sparseArray = [];
 
     private readonly List<(int, T)> denseArray = [];
 
@@ -24,23 +26,25 @@ public sealed class SparseMap<T> where T : AsInt
     public void Add(T value)
     {
         var id = value.AsInt();
-        var denseIndex = denseArray.Count;
 
         // 确保sparseArray足够大以容纳新的id
         if (id >= sparseArray.Count)
         {
-            sparseArray.AddRange(Enumerable.Repeat(-1, id - sparseArray.Count + 1));
+            sparseArray.Resize(id + 1, -1);
         }
 
-        // 如果这个id已经存在，先移除旧值
+        // 如果这个id已经存在，直接替换旧值而不改变数组结构
         if (sparseArray[id] != -1)
         {
-            RemoveAt(sparseArray[id]);
+            var existingIndex = sparseArray[id];
+            denseArray[existingIndex] = (id, value);
+
+            return;
         }
 
         // 添加新值到denseArray并更新sparseArray
         denseArray.Add((id, value));
-        sparseArray[id] = denseIndex;
+        sparseArray[id] = denseArray.Count - 1;
     }
 
     /// <summary>
@@ -94,7 +98,7 @@ public sealed class SparseMap<T> where T : AsInt
     /// <param name="id">要获取的值的id</param>
     /// <param name="value">获取到的值</param>
     /// <returns>如果找到指定id的值则返回true，否则返回false</returns>
-    public bool TryGetValue(int id, out T value)
+    public bool TryGetValue(int id, [MaybeNullWhen(false)] out T value)
     {
         value = default;
         if (id < 0 || id >= sparseArray.Count || sparseArray[id] == -1)
@@ -110,6 +114,15 @@ public sealed class SparseMap<T> where T : AsInt
     /// 获取映射中的元素数量
     /// </summary>
     public int Count => denseArray.Count;
+
+#endregion
+
+#region IDisposable method
+
+    public void Dispose()
+    {
+        sparseArray.Dispose();
+    }
 
 #endregion
 }
